270958363189462d81cdfadb5f207bd7bfc27714
egold555 <eric@golde.org>
2018-03-31 22:13:56 -0700

    ALL THE SWITCHES WORK!

--- PinballMachine.ino
+++ PinballMachine.ino
@@ -1,8 +1,8 @@
 
 const int DELAY = 80;
 const int LED = 13;
-
 const int  SWITCH_READ_DELAY = 5000;
+const int ANALOG_THRESHOLD = 100;
 
 const int OUT_SLINGSHOT_LEFT = 22;
 const int OUT_BALL_RETUN = 23;
@@ -12,15 +12,15 @@ const int OUT_THUMPER_RIGHT = 26;
 
 const int OUT_MX0 = 28;
 const int OUT_MX1 = 29;
-const int OUT_MX2 = ;
+const int OUT_MX2 = 30;
 const int OUT_MX3 = 31;
 const int OUT_MX4 = 32;
 const int OUT_MX5 = 33;
 
-const int IN_SW0 = 36;
-const int IN_SW1 = 37;
-const int IN_SW2 = 38;
-const int IN_SW3 = 39;
+const int IN_SW0 = A12; //36
+const int IN_SW1 = A13; //37
+const int IN_SW2 = A14; //38
+const int IN_SW3 = A15; //39
 
 bool mx0_sw0 = false;
 bool mx0_sw1 = false;
@@ -83,8 +83,15 @@ void loop() {
   //  pulse(OUT_BALL_RETUN);
   //  delay(3000);
   checkSwitches();
+  //solinoids();
   debug();
   delay(3000);
+}
+
+void solinoids(){
+  if(mx4_sw0){
+    pulse(OUT_SLINGSHOT_LEFT);
+  }
 }
 
 void checkSwitches() {
@@ -92,56 +99,60 @@ void checkSwitches() {
   delayMicroseconds(SWITCH_READ_DELAY);
   digitalWrite(OUT_MX0, HIGH);
   delayMicroseconds(SWITCH_READ_DELAY);
-  mx0_sw0 = (bool) digitalRead(IN_SW0);
-  mx0_sw1 = (bool) digitalRead(IN_SW1);
-  mx0_sw2 = (bool) digitalRead(IN_SW2);
-  mx0_sw3 = (bool) digitalRead(IN_SW3);
+  mx0_sw0 = analogRead(IN_SW0) > ANALOG_THRESHOLD;
+  mx0_sw1 = analogRead(IN_SW1) > ANALOG_THRESHOLD;
+  mx0_sw2 = analogRead(IN_SW2) > ANALOG_THRESHOLD;
+  mx0_sw3 = analogRead(IN_SW3) > ANALOG_THRESHOLD;
   digitalWrite(OUT_MX0, LOW);
 
 
   delayMicroseconds(SWITCH_READ_DELAY);
   digitalWrite(OUT_MX1, HIGH);
   delayMicroseconds(SWITCH_READ_DELAY);
-  mx1_sw0 = (bool) digitalRead(IN_SW0);
-  mx1_sw1 = (bool) digitalRead(IN_SW1);
-  mx1_sw2 = (bool) digitalRead(IN_SW2);
-  mx1_sw3 = (bool) digitalRead(IN_SW3);
+  mx1_sw0 = analogRead(IN_SW0) > ANALOG_THRESHOLD;
+  mx1_sw1 = analogRead(IN_SW1) > ANALOG_THRESHOLD;
+  mx1_sw2 = analogRead(IN_SW2) > ANALOG_THRESHOLD;
+  mx1_sw3 = analogRead(IN_SW3) > ANALOG_THRESHOLD;
   digitalWrite(OUT_MX1, LOW);
 
   delayMicroseconds(SWITCH_READ_DELAY);
   digitalWrite(OUT_MX2, HIGH);
   delayMicroseconds(SWITCH_READ_DELAY);
-  mx2_sw0 = (bool) digitalRead(IN_SW0);
-  mx2_sw1 = (bool) digitalRead(IN_SW1);
-  mx2_sw2 = (bool) digitalRead(IN_SW2);
-  mx2_sw3 = (bool) digitalRead(IN_SW3);
+  mx2_sw0 = analogRead(IN_SW0) > ANALOG_THRESHOLD;
+  mx2_sw1 = analogRead(IN_SW1) > ANALOG_THRESHOLD;
+  mx2_sw2 = analogRead(IN_SW2) > ANALOG_THRESHOLD;
+  mx2_sw3 = analogRead(IN_SW3) > ANALOG_THRESHOLD;
   digitalWrite(OUT_MX2, LOW);
 
   delayMicroseconds(SWITCH_READ_DELAY);
   digitalWrite(OUT_MX3, HIGH);
   delayMicroseconds(SWITCH_READ_DELAY);
-  mx3_sw0 = (bool) digitalRead(IN_SW0);
-  mx3_sw1 = (bool) digitalRead(IN_SW1);
-  mx3_sw2 = (bool) digitalRead(IN_SW2);
-  mx3_sw3 = (bool) digitalRead(IN_SW3);
+  mx3_sw0 = analogRead(IN_SW0) > ANALOG_THRESHOLD;
+  mx3_sw1 = analogRead(IN_SW1) > ANALOG_THRESHOLD;
+  mx3_sw2 = analogRead(IN_SW2) > ANALOG_THRESHOLD;
+  mx3_sw3 = analogRead(IN_SW3) > ANALOG_THRESHOLD;
+  //Serial.print("33 RAW: ");
+  //Serial.println(analogRead(IN_SW3));
   digitalWrite(OUT_MX3, LOW);
 
   delayMicroseconds(SWITCH_READ_DELAY);
   digitalWrite(OUT_MX4, HIGH);
   delayMicroseconds(SWITCH_READ_DELAY);
-  mx4_sw0 = (bool) digitalRead(IN_SW0);
-  mx4_sw1 = (bool) digitalRead(IN_SW1);
-  mx4_sw2 = (bool) digitalRead(IN_SW2);
-  mx4_sw3 = (bool) digitalRead(IN_SW3);
+  mx4_sw0 = analogRead(IN_SW0) > ANALOG_THRESHOLD;
+ // Serial.print("40 RAW: ");
+  //Serial.println(analogRead(IN_SW0));
+  mx4_sw1 = analogRead(IN_SW1) > ANALOG_THRESHOLD;
+  mx4_sw2 = analogRead(IN_SW2) > ANALOG_THRESHOLD;
+  mx4_sw3 = analogRead(IN_SW3) > ANALOG_THRESHOLD;
   digitalWrite(OUT_MX4, LOW);
 
   delayMicroseconds(SWITCH_READ_DELAY);
   digitalWrite(OUT_MX5, HIGH);
   delayMicroseconds(SWITCH_READ_DELAY);
-  mx5_sw0 = (bool) digitalRead(IN_SW0);
-  mx5_sw1 = (bool) digitalRead(IN_SW1);
-  mx5_sw2 = (bool) digitalRead(IN_SW2);
-  mx5_sw3 = (bool) digitalRead(IN_SW3);
+  mx5_sw0 = analogRead(IN_SW0) > ANALOG_THRESHOLD;
+  mx5_sw1 = analogRead(IN_SW1) > ANALOG_THRESHOLD;
+  mx5_sw2 = analogRead(IN_SW2) > ANALOG_THRESHOLD;
+  mx5_sw3 = analogRead(IN_SW3) > ANALOG_THRESHOLD;
   digitalWrite(OUT_MX5, LOW);
 
 
